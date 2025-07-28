1bcd06305a4e121e01435d18dfeab149fdea54a4
durgamoorthy <79762498+durgamoorthy@users.noreply.github.com>
2025-07-28 16:09:40 -0500

    Removed temporary files

--- temp.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-csv_file = np.genfromtxt(r"c:\Users\super\Downloads\my_Eye_Gaze_Transforms_20250728_143443_2.csv", delimiter=",")
-print(csv_file)
-ts = csv_file[:, -1]
-dts = np.diff(ts)
-print(np.where(dts > 1000))
-
