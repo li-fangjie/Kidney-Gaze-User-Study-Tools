82f785b79023e63963746684573f4bd5b6ca01fe
Beeper-Weepers <lukasvantilburg@gmail.com>
2025-08-21 15:03:41 -0500

    Changed update rate

--- Experiment Controller/server.py
+++ Experiment Controller/server.py
@@ -142,7 +142,7 @@ class HoloLensCoordinatorApp(QWidget):
     
     # ──────────────── EYE GAZE ────────────────
     def start_gaze_timer(self):
-        self.send_timer.start(100)
+        self.send_timer.start(32)
 
     def send_published_eye_gaze(self):
         # Send without retry
@@ -232,7 +232,7 @@ class ZMQListenerThread(QObject):
     def run(self):
 
         while self.running:
-            socks = dict(self.poller.poll(timeout=32))  # 100ms timeout
+            socks = dict(self.poller.poll(timeout=100))  # 100ms timeout
             if self.router in socks:
                 msg = self.router.recv_multipart()
                 identity, content = msg
